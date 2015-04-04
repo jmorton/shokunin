e6a83f30f859fa2e4d193fbaf2a59d02b5ed0872
Jonathan Morton <jmorton@element84.com>
2015-04-03 23:21:32 -0500

    Separate file reading from analysis while still being lazy.

--- src/shokunin/word.clj
+++ src/shokunin/word.clj
@@ -1,22 +1,31 @@
-(ns shokunin.words
+(ns shokunin.word
   (:require [clojure.string :refer [lower-case split-lines]]))
 
 
 (defn term-frequency
-  "Count occurrences of terms in a file."
-  [file stop-set]
-  (with-open [rdr (clojure.java.io/reader file)]
-    (let [tokenize (partial re-seq #"(?:\w[\w\-\']+)")
-          non-stop (partial remove stop-set)
-          parse    (comp frequencies non-stop tokenize lower-case)
-          tally    (map parse (line-seq rdr))
-          adder    (partial merge-with +)]
-      (reduce adder tally))))
+  "Count occurrences of terms in a sequence of string."
+  [lines stop-set]
+  (let [tokenize (partial re-seq #"(?:\w[\w\-\']+)")
+        non-stop (partial remove stop-set)
+        parse    (comp frequencies non-stop tokenize lower-case)
+        tally    (map parse lines)
+        adder    (partial merge-with +)]
+    (reduce adder tally)))
 
 
-(def stop-set (-> "resources/01/stop.txt" slurp split-lines set))
-(def freq-map (term-frequency "resources/01/declaration.txt" stop-set))
+(defn file-term-frequency
+  "Count occurrences of terms in a file.  Does not load
+   entire file into memory."
+  [term-path stop-path]
+  (with-open [file (clojure.java.io/reader term-path)]
+    (let [stop-set (-> stop-path slurp split-lines set)]
+    (term-frequency (line-seq file) stop-set))))
 
 
-(take 10 (sort-by last > freq-map))
-(take 10 (sort-by first compare freq-map))
+(defn example []
+  (let [tf (file-term-frequency "resources/01/declaration.txt"
+                                "resources/01/stop.txt")]
+    (println (take 10 (sort-by last > tf)))
+    (println (take 10 (sort-by first compare tf)))))
+
+(example)
