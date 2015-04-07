ac1fb8639205579d5d377bb1b7172aa816f421c7
Jon Morton <jon.morton@gmail.com>
2015-04-06 20:09:34 -0500

    Work prime number testers/generators

--- src/shokunin/euler.clj
+++ src/shokunin/euler.clj
@@ -20,3 +20,22 @@
   ([x y] (cons x (lazy-seq (fib-seq y (+ x y))))))
 
 (def answer-2 (reduce + (filter even? (take-while (fn [x] (< x 4000000)) (fib-seq)))))
+
+;; prime number tools, useful for subsequent problems.
+
+;;; a super naive and expensive check
+(defn naive-prime? [n]
+  (cond
+   (< n 2) false
+   (= n 2) true
+   (even? n) false
+   :else (not-any? #(= 0 (rem n %)) (range 3 (Math/sqrt n) 2))))
+
+;; eratosthenes seive. does not work for large primes because the stack
+;; gets way too deep.
+(defn dirty-sieve
+  ([] (sieve (iterate inc 2)))
+  ([s]
+  (cons (first s)
+        (lazy-seq (sieve (filter #(not= 0 (rem % (first s)))
+                                 (rest s)))))))
