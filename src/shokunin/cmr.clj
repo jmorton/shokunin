6cb25053f9ca6f584a6022ef87c562d9e94cba6e
Jon Morton <jon.morton@gmail.com>
2015-04-13 23:06:21 -0500

    Contemplating lazy http requests

--- src/shokunin/cmr.clj
+++ src/shokunin/cmr.clj
@@ -2,14 +2,27 @@
   (:require [clj-http.client :as client]
             [clojure.data.json :as json]))
 
-(def opts {:accept :json
-           :query-params {:short-name "MOD09A1"}})
-(def resp (client/get "https://cmr.earthdata.nasa.gov/search/granules" opts))
+(defn search [url opts]
+  (let [defaults {:accept :json }
+        opts+    (merge-with merge defaults opts)
+        resp     (client/get url opts+)
+        body     (client/json-decode (resp :body))]
+    body))
 
-(-> resp :headers :cmr-hits)
+(-> (search url opts) (get-in ["feed" "entry"] first))
 
-(def body (client/json-decode (:body resp)))
+(defn http-seq [url opts]
+  (let [entries (-> (search url opts) (get-in ["feed" "entry"]))]
+    ; the last item in the seq should somehow invoke
+    ; a subsequent call to get more items if more
+    ; results exist. however, the search function only
+    ; returns a body... there is no information about
+    ; where in the result list we actually are. hmm.
+    entries))
 
-(def results (get-in body ["feed" "entry"]))
-
-(first (get (first results) "links"))
+(comment
+  ; an example
+  (def opts { :query-params {:short-name "MOD09A1" :page-num 1 :page-size 5}})
+  (def url "https://cmr.earthdata.nasa.gov/search/granules")
+  (def s (http-seq url opts))
+  (count s))
