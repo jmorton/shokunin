8d6d2b9173ebc57f1e13cb5c60d9029f72727eb8
Jon Morton <jon.morton@gmail.com>
2015-04-14 08:12:56 -0500

    In progress commit

--- src/shokunin/cmr.clj
+++ src/shokunin/cmr.clj
@@ -1,28 +1,51 @@
 (ns shokunin.cmr
   (:require [clj-http.client :as client]
-            [clojure.data.json :as json]))
+            [clojure.data.json :as json]
+            [clojure.string :as string]))
 
-(defn search [url opts]
+(defn search [url params]
   (let [defaults {:accept :json }
-        opts+    (merge-with merge defaults opts)
-        resp     (client/get url opts+)
+        params+  (merge-with merge defaults params)
+        resp     (client/get url params+)
         body     (client/json-decode (resp :body))]
     body))
 
-(-> (search url opts) (get-in ["feed" "entry"] first))
+(defn hits
+  ""
+  [url params]
+  (let [response (client/head url params)
+        cmr-hits (get-in response [:headers "CMR-hits"])]
+    (or (Integer/parseInt cmr-hits) 0)))
 
-(defn http-seq [url opts]
-  (let [entries (-> (search url opts) (get-in ["feed" "entry"]))]
-    ; the last item in the seq should somehow invoke
-    ; a subsequent call to get more items if more
-    ; results exist. however, the search function only
-    ; returns a body... there is no information about
-    ; where in the result list we actually are. hmm.
-    entries))
+(defn search
+  ""
+  [url params]
+  (let [defaults {:accept :json }
+        params+  (merge-with merge defaults params)
+        resp     (client/get url params+)
+        body     (client/json-decode (resp :body))]
+    body))
 
-(comment
-  ; an example
-  (def opts { :query-params {:short-name "MOD09A1" :page-num 1 :page-size 5}})
-  (def url "https://cmr.earthdata.nasa.gov/search/granules")
-  (def s (http-seq url opts))
-  (count s))
+(defn http-seq
+  ""
+  ;; this doesn't *set* defaults in the map... careful.
+  ([url {{:keys [page-num page-size]} :query-params
+          :or {page-num 1 page-size 10}
+          :as params}]
+     (let [last-page (int (Math/ceil (/ (hits url params) page-size)))
+           opts {:last-page last-page}]
+       (http-seq url params opts)))
+  ([url {{:keys [page-num page-size]} :query-params :as params}
+        {:keys [last-page] :as opts}]
+       (let [entries     (get-in (search url params) ["feed" "entries"])
+             next-params (update-in params [:query-params :page-num] inc)]
+         (<= page-num last-page)
+         (concat entries (lazy-seq (http-seq url next-params opts))))))
+
+; an example
+(def params { :query-params {:short-name "MOD09A1" :page-num 1 :page-size 10}})
+(def opts {:last-page 10})
+(def url "https://cmr.earthdata.nasa.gov/search/granules")
+(def result (http-seq url params opts))
+(map #(get % "title") (take 2 result))
+
