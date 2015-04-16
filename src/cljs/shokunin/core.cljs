fb4a36a44230255c014b99c0ea55065b4257f6d5
Jon Morton <jon.morton@gmail.com>
2015-04-15 22:13:26 -0500

    Add working html page and println to example

--- src/cljs/shokunin/core.cljs
+++ src/cljs/shokunin/core.cljs
@@ -1,10 +1,11 @@
 (ns shokunin.core
   (:require [clojure.browser.repl :as repl]))
 
+(enable-console-print!)
+(print "Ahoy!")
+
 (defonce conn
   (repl/connect "http://localhost:9000/repl"))
-
-(enable-console-print!)
 
 (defn ahoy [x]
   (str "Ahoy, " x))
