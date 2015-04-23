5522c7dfb6a0222c17cb277d8a7be47ea2bf542d
Jon Morton <jon.morton@gmail.com>
2015-04-22 22:22:55 -0500

    Silly, but instructive fizz-buzz variant

--- src/shokunin/fizzbuzz.clj
+++ src/shokunin/fizzbuzz.clj
@@ -1,7 +1,14 @@
 (ns shokunin.fizzbuzz)
 
-(def fizz-buzz (let [number (iterate inc 1)
+(def fizz-buzz-seq (let [number (iterate inc 1)
                      fizzer (cycle [nil nil "fizz"])
                      buzzer (cycle [nil nil nil nil "buzz"])]
                  (map #(or (if (or %2 %3) (str %2 %3)) %1)
                       number fizzer buzzer)))
+
+(def fizz-buzz-fn
+  (some-fn #(and (= (rem % 3) 0) (= (mod % 5) 0) "FizzBuzz")
+           #(and (= (rem % 3) 0) "Fizz")
+           #(and (= (rem % 5) 0) "Buzz")))
+
+
